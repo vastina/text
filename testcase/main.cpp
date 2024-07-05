6f21128979a6ca348e778af67fca8f83efaa4d6a
vastina <719583581@qq.com>
2024-07-05 15:11:53 +0800

    get_dpi

--- testcase/main.cpp
+++ testcase/main.cpp
@@ -4,6 +4,40 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
+#ifdef _WIN32
+#include <windows.h>
+#elif __APPLE__
+#error "no support on apple"
+#else
+#include <X11/Xlib.h>
+#endif
+void GetScreenDPI(int& dpiX, int& dpiY) {
+#ifdef _WIN32
+    HDC screen = GetDC(NULL);
+    dpiX = GetDeviceCaps(screen, LOGPIXELSX);
+    dpiY = GetDeviceCaps(screen, LOGPIXELSY);
+    ReleaseDC(NULL, screen);
+#elif __APPLE__
+    return;
+#elif __linux__
+    Display* display = XOpenDisplay(NULL);
+    if (display == NULL) {
+        std::cerr << "Cannot open display" << std::endl;
+        dpiX = dpiY = -1; // Return invalid DPI values
+        return;
+    }
+    int screen_width = XDisplayWidth(display, DefaultScreen(display));
+    int screen_height = XDisplayHeight(display, DefaultScreen(display));
+    int screen_width_mm = XDisplayWidthMM(display, DefaultScreen(display));
+    int screen_height_mm = XDisplayHeightMM(display, DefaultScreen(display));
+    dpiX = (int)(25.4 * screen_width / screen_width_mm);
+    dpiY = (int)(25.4 * screen_height / screen_height_mm);
+    XCloseDisplay(display);
+#else
+    dpiX = dpiY = -1; // Unsupported platform
+#endif
+}
+
 #include "png.hpp"
 
 #include <chrono>
@@ -24,6 +58,8 @@ int main(int argc, char* argv[]) {
   unsigned h = 0;
   unsigned w[7] = { 0 };
 
+  int dpiX, dpiY;
+  GetScreenDPI(dpiX, dpiY);
   FT_Face face[7];
   const char name[] { "vastina" };
   FT_Bitmap* bitmaps[7];
@@ -33,7 +69,7 @@ int main(int argc, char* argv[]) {
       FT_Done_FreeType( library );
       return 1;
     }
-    FT_Set_Char_Size( face[i], 0, 48 * 64, 0, 0 );
+    FT_Set_Char_Size( face[i], 0, 48 * 64, dpiX, dpiY);
     if ( FT_Load_Char( face[i], name[i], FT_LOAD_RENDER ) ) {
       std::cerr << "Could not load character" << name[i] << std::endl;
       FT_Done_Face( face[i] );
