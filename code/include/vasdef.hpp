34baa18c57c05682a5f968157af2c54c53f7bae9
vastina <719583581@qq.com>
2024-07-05 17:10:04 +0800

    update vasdef.hpp

--- code/include/vasdef.hpp
+++ code/include/vasdef.hpp
@@ -12,19 +12,20 @@ using std::string;
 using std::string_view;
 using std::vector;
 using u64 = std::uint64_t;
+using i64 = std::int64_t;
 using u32 = std::uint32_t;
 using i32 = std::int32_t;
 using u16 = std::uint16_t;
 using u8 = unsigned char;
 
-#define GSetter( member )                                                                                          \
-  decltype( member )& get##member()                                                                                \
-  {                                                                                                                \
-    return member;                                                                                                 \
-  }                                                                                                                \
-  void set##member( const decltype( member )& _##member )                                                          \
-  {                                                                                                                \
-    (member) = _##member;                                                                                            \
+#define GSetter( member )                                                                                              \
+  decltype( member )& get##member()                                                                                    \
+  {                                                                                                                    \
+    return member;                                                                                                     \
+  }                                                                                                                    \
+  void set##member( const decltype( member )& _##member )                                                              \
+  {                                                                                                                    \
+    ( member ) = _##member;                                                                                            \
   }
 
 }
